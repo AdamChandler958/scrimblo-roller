00c9d5c0c6082e8b1bfbe69f1525f6f8416824ff
Adam Chandler <adam.c02pro@gmail.com>
2025-09-03 16:48:50 +0930

    feat: extremely basic discord bot

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from scrimblo-roller!")
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 
-if __name__ == "__main__":
-    main()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print (f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('hello bot'):
+        await message.channel.send("Hello!")
+
+
+client.run(API_KEY)
+
+
