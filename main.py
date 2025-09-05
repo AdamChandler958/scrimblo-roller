8dc200ab2e71d53873678243566f39a66daecf6c
Adam Chandler <adam.c02pro@gmail.com>
2025-09-05 20:37:11 +0930

    feat: adding simple test slash command

--- main.py
+++ main.py
@@ -14,18 +14,28 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+tree = discord.app_commands.CommandTree(client)
+
 
 @client.event
 async def on_ready():
     print (f"We have logged in as {client.user}")
+    await tree.sync()
+    print("commands synced!")
+
+
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.message.Message):
     if message.author == client.user:
         return
     
     if message.content.startswith('hello bot'):
         await message.channel.send("Hello!")
+
+@tree.command(name="test", description="Test if bot is working")
+async def test_command(interaction: discord.Interaction):
+    await interaction.response.send_message("Slash command is working")
 
 print(get_secret("api_key"))
 client.run(get_secret("api_key"))
