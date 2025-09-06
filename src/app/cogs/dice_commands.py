f4ccc7b726bfdba1b1c41a6d7f13af1e1da919f4
Adam Chandler <adam.c02pro@gmail.com>
2025-09-06 14:31:45 +0930

    feat: adding regex to dice parsing

--- src/app/cogs/dice_commands.py
+++ src/app/cogs/dice_commands.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from src.app.rolls import dice_roll
+from src.app.rolls import parse_dice_string
 
 class DiceCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -8,9 +8,8 @@ class DiceCommands(commands.Cog):
 
     @discord.app_commands.command(name="roll", description="Roll some dice!")
     @discord.app_commands.describe(roll="The size and number of dice you wish to roll [Int currently]")
-    async def roll_command(self, interaction: discord.Interaction, roll: int):
-        value = dice_roll(roll)
-        await interaction.response.send_message(f"Your roll is:\n ({value})")
+    async def roll_command(self, interaction: discord.Interaction, roll: str):
+        await interaction.response.send_message(f"{roll}:\n ```{parse_dice_string(roll)}```")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(DiceCommands(bot))
--- src/app/rolls.py
+++ src/app/rolls.py
@@ -1,6 +1,39 @@
 import random
+import re
 
-def dice_roll(dice_size: int):
+def dice_roll(dice_size: str) -> int:
 
-    val = random.randint(1, dice_size)
+    val = random.randint(1, int(dice_size))
     return val
+
+# /roll 3d20+2
+
+# [8, 16, 20]+2 = 46
+
+def parse_dice_string(request: str):
+    pattern = re.compile(r"(\d*)d(\d+)(?:([+-])(\d+))?")
+    match = pattern.match(request)
+
+    if not match:
+        return "Please use either NdN or NdN+M format"
+
+    num_dice_str, die_type_str, modifier_op, modifier_val_str = match.groups()
+
+    roll_list = []
+    for _ in range(int(num_dice_str)):
+        roll_list.append(dice_roll(die_type_str))
+
+    if modifier_op is not None:
+        if modifier_op == "+":
+            mod_mult = 1
+
+        else:
+            mod_mult = -1
+
+    sum_value = sum(roll_list) + (mod_mult*int(modifier_val_str))
+
+    roll_strings= [str(roll) for roll in roll_list]
+    rolls_display = f"[{', '.join(roll_strings)}]"
+
+    return f"{rolls_display} {modifier_op}{modifier_val_str} = {sum_value}"
+
