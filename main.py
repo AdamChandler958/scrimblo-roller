6b14843086c65ee5c7b841913bec45499003c071
Adam Chandler <adam.c02pro@gmail.com>
2025-11-30 08:15:10 +1030

    fix: updated dev environ

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ import os
 from discord.ext import commands
 import pathlib
 
-from src.app.utils import get_secret
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-IS_DEV = os.getenv('IS_DEV', 'True').lower() in ('true', '1', 't')
-DEV_GUILD_WHITELIST = [1312692823820210216] # Test server id
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 async def load_cogs():
     cogs_path = pathlib.Path("src/app/cogs")
@@ -33,23 +30,12 @@ async def load_cogs():
                 print(f"Failed to load extension {module_name} with error: {e}")
 
 
-
 @bot.event
 async def on_ready():
-    print (f"We have logged in as {bot.user}")
-    if IS_DEV:
-        for guild in bot.guilds:
-            if guild.id in DEV_GUILD_WHITELIST:
-
-                await load_cogs()
-                synced = await bot.tree.sync()
-                print(f"Synced {len(synced)} command(s)!")
-                
-    else:
-        await load_cogs()
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)!")
-
-bot.run(get_secret("api_key"))
+    print(f"We have logged in as {bot.user}")
+    await load_cogs()
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} command(s)!")
 
 
+bot.run(API_KEY)
